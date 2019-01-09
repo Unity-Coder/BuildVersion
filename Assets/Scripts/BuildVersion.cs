d0877f9acad31be61c8e02e00117117280b3ecda
UnityCoder <pfreby@hotmail.com>
2019-01-09 11:46:52 +0100

    Initial

--- Assets/Scripts/BuildVersion.cs
+++ Assets/Scripts/BuildVersion.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class BuildVersion : MonoBehaviour
+namespace BroodStudios
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public class BuildVersion : MonoBehaviour
+	{
+		[SerializeField, HideInInspector]
+		private Text _textBuilVersion = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		[SerializeField]
+		private string _textVersion;
+
+		[SerializeField]
+		private Color _textColor = Color.yellow;
+
+		[SerializeField, Range(8,64)]
+		private int _fontSize = 14;
+
+		[SerializeField]
+		private AlignBuildVersion _screenPosition = AlignBuildVersion.TopLeft;
+
+
+
+		private void Awake()
+		{
+			UpdateValues();
+		}
+
+		private void OnValidate()
+		{
+			UpdateValues();
+		}
+
+		private void UpdateValues()
+		{
+			if (_textBuilVersion)
+			{
+				_textBuilVersion.text = GetTextVersion();
+				_textBuilVersion.color = _textColor;
+				_textBuilVersion.fontSize = _fontSize;
+
+			}
+		}
+
+		private string GetTextVersion()
+		{
+			if (_textVersion.Trim() != "")
+				return _textVersion;
+			else
+				return "Void"; 
+		}
+	}
 }
